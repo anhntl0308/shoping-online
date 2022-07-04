69b1f5c3850a0b70b3823908b4b5094c3f384d5e
anhntl0308 <anhntl0308@gmail.com>
2022-07-05 00:53:15 +0700

    show list and add category

--- src/main/java/edu/lanh/shop/model/CategoryDto.java
+++ src/main/java/edu/lanh/shop/model/CategoryDto.java
@@ -9,32 +9,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryDto{
 	private Long categoryid;
 	@NotEmpty
 	@Min(value = 5)
 	private String categoryname;
-	public CategoryDto(Long categoryid, @NotEmpty @Min(5) String categoryname) {
-		super();
-		this.categoryid = categoryid;
-		this.categoryname = categoryname;
-	}
-	public CategoryDto() {
-		super();
-	}
-	public Long getCategoryid() {
-		return categoryid;
-	}
-	public void setCategoryid(Long categoryid) {
-		this.categoryid = categoryid;
-	}
-	public String getCategoryname() {
-		return categoryname;
-	}
-	public void setCategoryname(String categoryname) {
-		this.categoryname = categoryname;
-	}
-	
 	
 	
 }
